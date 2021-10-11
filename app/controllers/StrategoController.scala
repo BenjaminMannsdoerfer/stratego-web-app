9c4a0b8738969287b1234200945f4f429d88cb9d
Benjamin Mannsdörfer <b.mannsdoerfer@gmx.de>
2021-10-11 17:13:17 +0200

    most part done

--- app/controllers/StrategoController.scala
+++ app/controllers/StrategoController.scala
@@ -10,37 +10,74 @@ import de.htwg.se.stratego.controller.controllerComponent.GameStatus
 class StrategoController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   val gameController = Stratego.controller
 
-  def save = gameController.save
+  def printStratego = gameController.matchFieldToString + GameStatus.getMessage(gameController.gameStatus)
 
-  def load = gameController.load
-
-  def redo = gameController.redo
-
-  def undo = gameController.undo
-
-  def strategoAsText = gameController.matchFieldToString + GameStatus.getMessage(gameController.gameStatus)
-
-  def about = Action {
+  def home = Action {
     Ok(views.html.index())
   }
 
+  def setPlayer(player1: String, player2: String) = Action {
+    gameController.setPlayers(player1 + " " + player2)
+    Ok("Hello " + gameController.playerList(0) +
+      " and " + gameController.playerList(1) + "!")
+  }
+
+  def init = Action {
+    gameController.initMatchfield
+    Ok(printStratego)
+  }
+
+  def setCharacter(row:Int, col:Int, charac:String) = Action {
+    gameController.set(row,col,charac)
+    Ok(printStratego)
+  }
+
+  def move(dir:Char,row:Int,col:Int) = Action {
+    gameController.move(dir, row, col)
+    Ok(printStratego)
+  }
+
+  def attack (rowA:Int, colA:Int, rowD:Int, colD:Int) = Action {
+    gameController.attack(rowA,colA, rowD, colD)
+    Ok(printStratego)
+  }
+
   def stratego = Action {
-    Ok(strategoAsText)
+    Ok(printStratego)
   }
 
   def saveGame = Action {
-    Ok(save)
+    gameController.save
+    Ok(printStratego)
   }
 
   def loadGame = Action {
-    Ok(load)
+    gameController.load
+    Ok(printStratego)
   }
 
   def undoGame = Action {
-    Ok(undo)
+    gameController.undo
+    Ok(printStratego)
   }
 
   def redoGame = Action {
-    Ok(redo)
+    gameController.redo
+    Ok(printStratego)
+  }
+
+  def smallGame = Action {
+    val smallMatchfield = gameController.createNewMatchfieldSize(4)
+    Ok(smallMatchfield)
+  }
+
+  def mediumGame = Action {
+    val mediumMatchfield = gameController.createNewMatchfieldSize(7)
+    Ok(mediumMatchfield)
+  }
+
+  def largeGame = Action {
+    val largeMatchfield = gameController.createNewMatchfieldSize(10)
+    Ok(largeMatchfield)
   }
 }
